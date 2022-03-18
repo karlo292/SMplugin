a925b59702b439580a94ec64fe7ecad9d0dad41c
karlo292 <karlito1501@gmail.com>
2022-03-18 10:43:22 +0100

    Made /playtime command with /playtime and /playtime <player>

--- src/main/java/me/karlo292/testplugin/commands/PlaytimeCommand.java
+++ src/main/java/me/karlo292/testplugin/commands/PlaytimeCommand.java
@@ -1,10 +1,18 @@
 package me.karlo292.testplugin.commands;
 
 import me.karlo292.testplugin.customConfig.customPlayerConfig;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 public class PlaytimeCommand implements CommandExecutor {
     @Override
@@ -12,16 +20,31 @@ public class PlaytimeCommand implements CommandExecutor {
         if(sender instanceof Player p){
             if(args.length == 0){
                 customPlayerConfig.setup();
-                int playtimeDays = (int) customPlayerConfig.get().getInt("playtimeDays");
+                int playtimeDays =  (int) customPlayerConfig.get().getInt("playtimeDays");
                 int playtimeHours = (int) customPlayerConfig.get().getInt("playtimeHours");
-                int playtimeMinutes = (int) customPlayerConfig.get().get("playtimeMinutes");
-                int playtimeSeconds = (int) customPlayerConfig.get().get("playtimeSeconds");
-                p.sendMessage("Your playtime is " + playtimeDays + " days, " + playtimeHours + " hours, " + playtimeMinutes + " minutes and " + playtimeSeconds + " seconds");
-                /*p.sendMessage("Days: " + playtimeDays);
-                p.sendMessage("Hours: " + playtimeHours);
-                p.sendMessage("Minutes: " + playtimeMinutes);
-                p.sendMessage("Seconds: " + playtimeSeconds);*/
+                int playtimeMinutes = (int) customPlayerConfig.get().getInt("playtimeMinutes");
+                int playtimeSeconds = (int) customPlayerConfig.get().getInt("playtimeSeconds");
+                p.sendMessage(ChatColor.GREEN + "Your playtime is " + playtimeDays + "d, " + playtimeHours + "h, " + playtimeMinutes + "m and " + playtimeSeconds + "s");
+                return true;
 
+            }else{
+                if(Bukkit.getPlayer(args[0]) == null){
+                    p.sendMessage(ChatColor.RED + "That player never played on server before");
+                    return true;
+                }
+                UUID targetPlayerUUID = Bukkit.getServer().getPlayer(args[0]).getUniqueId();
+                File file = new File(Bukkit.getServer().getPluginManager().getPlugin("TestPlugin").getDataFolder(), "PlayerDatabase" + File.separator + targetPlayerUUID.toString());
+
+                if(!file.exists()){
+                    p.sendMessage(ChatColor.RED + "That player never played on server before");
+                    return true;
+                }
+                FileConfiguration customFile = YamlConfiguration.loadConfiguration(file);
+                int playtimeDays =  (int) customFile.getInt("playtimeDays");
+                int playtimeHours = (int) customFile.getInt("playtimeHours");
+                int playtimeMinutes = (int) customFile.getInt("playtimeMinutes");
+                int playtimeSeconds = (int) customFile.getInt("playtimeSeconds");
+                p.sendMessage(ChatColor.GREEN + "Playtime of " + args[0] + " is " + playtimeDays + "d, " + playtimeHours + "h, " + playtimeMinutes + "m and " + playtimeSeconds + "s");
             }
         }
 
--- src/main/java/me/karlo292/testplugin/customConfig/customPlayerConfig.java
+++ src/main/java/me/karlo292/testplugin/customConfig/customPlayerConfig.java
@@ -5,15 +5,16 @@ import me.karlo292.testplugin.listeners.JoinLeaveListener;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 public class customPlayerConfig {
     private static File file;
     private static FileConfiguration customFile;
-
     Plugin plugin = TestPlugin.plugin;
 
     //FINDS OR GENERATES CUSTOM CONFIG FILE
@@ -29,8 +30,10 @@ public class customPlayerConfig {
         }
         customFile = YamlConfiguration.loadConfiguration(file);
 
-
     }
+
+
+
     public static FileConfiguration get(){
         return customFile;
     }
